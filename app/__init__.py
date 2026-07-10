b46980d4caf0bddd4f274dcf3a9bae79e402a5f3
mohamad-755 <mohammadziadah212@gmail.com>
2026-07-10 15:04:04 +0300

    Add SQLite database initialization

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,9 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    from app import db
+    db.init_app(app)
+
     from app.routes import main
     app.register_blueprint(main)
 
